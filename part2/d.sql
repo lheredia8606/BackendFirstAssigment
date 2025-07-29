d79aed5e65db3d1b4780d3968843cb2e5f5e9a01
Leandro <lheredia8606@gmail.com>
2025-07-29 19:43:30 -0400

    removed hard code value

--- part2/d.sql
+++ part2/d.sql
@@ -1,3 +1,8 @@
 -- Below is a dummy query, if we leave it empty then the SQLITE package will throw a non-readable error
 -- Change the query to pass the test :)
-SELECT first_name, last_name from users JOIN favorites ON users.id = favorites.user_id where favorites.dog_id = 2;
+SELECT
+ users.first_name, users.last_name 
+FROM favorites 
+JOIN users ON users.id = favorites.user_id
+JOIN dogs on dogs.id = favorites.dog_id
+where dogs.name = 'Zoey';
